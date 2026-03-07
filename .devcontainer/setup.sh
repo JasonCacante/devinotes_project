73821aebb99132e5fce67de2e1f1fd40be2b4d58
JasonCacante <jason.cacante@gmail.com>
2026-03-07 02:35:45 +0000

    Refactor setup.sh to install oh-my-zsh and plugins conditionally
    - Added checks to prevent reinstallation of oh-my-zsh, Powerlevel10k, and zsh plugins.
    - Copied .p10k.zsh configuration file to the home directory.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,22 +8,31 @@ export PATH="$HOME/.local/bin:$PATH"
 # Instalar dependencias del proyecto
 uv sync
 
-# Instalar oh-my-zsh sin interacción
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Instalar oh-my-zsh sin interacción (solo si no existe)
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-# Instalar Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
-  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+# Instalar Powerlevel10k (solo si no existe)
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+fi
 
-# Instalar plugins de zsh
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git \
-  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+# Instalar plugins de zsh (solo si no existen)
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+fi
 
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git \
-  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+fi
 
 # Copiar .zshrc limpio (sin secrets)
 cp .devcontainer/.zshrc ~/.zshrc
+cp .devcontainer/.p10k.zsh ~/.p10k.zsh
 
 # Configurar zsh como shell por defecto
 sudo chsh -s "$(which zsh)" "$USER"
