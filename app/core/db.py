e9fc22d861da0e6cf57db900ce79964d33a1db78
JasonCacante <jason.cacante@gmail.com>
2026-03-09 13:04:52 -0500

    Disable SQLAlchemy echo in database engine configuration

--- app/core/db.py
+++ app/core/db.py
@@ -4,8 +4,6 @@ from sqlmodel import Session, SQLModel, create_engine
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
-
 engine = create_engine(
     settings.DATABASE_URL,
     echo=False,
